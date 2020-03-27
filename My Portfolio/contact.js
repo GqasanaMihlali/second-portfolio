f56120e690222bc3d3cbd740bb7b059d70d71946
Mihlali <59651776+GqasanaMihlali@users.noreply.github.com>
2020-03-27 14:45:08 +0200

    Add files via upload

--- My Portfolio/contact.js
+++ My Portfolio/contact.js
@@ -1,3 +1,21 @@
+// Firebase configuration
+var firebaseConfig = {
+     apiKey: "AIzaSyC71IIksBjm4A_rGh4QT-UQZBiG07VFtME",
+     authDomain: "my-portfolio-13fbf.firebaseapp.com",
+     databaseURL: "https://my-portfolio-13fbf.firebaseio.com",
+     projectId: "my-portfolio-13fbf",
+     storageBucket: "my-portfolio-13fbf.appspot.com",
+     messagingSenderId: "1015063904934",
+     appId: "1:1015063904934:web:7784aa0872606a91a7ea67",
+     measurementId: "G-C4R2X2MR1C"
+   };
+
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+   //Reference messages collection
+           var messagesRef = firebase.database().ref("messages"); 
+ 
 // Listen for submit 
 document.getElementById('Contact').addEventListener('submit', submitForm);
  
@@ -42,4 +60,19 @@ document.querySelector('.alert').style.display = 'block';
                return document.getElementById(id).value;
           }
         }
+        // Save message to firebase
+        function saveMessage(name, email, phone, message){
+          var newMessageRef = messagesRef.push();
+          newMessageRef.set({
+               name: name,
+               email: email,
+               phone: phone,
+               message:message
+
+          });
+      }
+
+   
+
+  
             
